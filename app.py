fa9775e306c631a2d2aff35c943a5454190f02ff
hemanthsai9 <krothapalli.21@gmail.com>
2025-09-09 15:34:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Load trained model
 try:
-    with open("trained_ai.pkl", "rb") as f:
+    with open("ai_model.pkl", "rb") as f:
         ai = pickle.load(f)
     logging.info("Model loaded successfully.")
 except FileNotFoundError:
@@ -20,7 +20,7 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     return "✅ AI Server Running"
-
+                                                     
 @app.route("/predict")
 def predict():
     if ai is None:
@@ -42,3 +42,4 @@ def predict():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use host-provided PORT
     app.run(host="0.0.0.0", port=port)
+
